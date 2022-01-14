06f44a2f3d14c6f7efd4f01340ed5c9b34db104e
Rahul Sharma <rahulsharma@Rahuls-MacBook-Pro.local>
2022-01-15 01:33:58 +0530

    choose selection to continue the game

---
+++ app.js
@@ -0,0 +1,26 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_VALUE = ROCK;
+let gameRunning = false;
+
+const getPlayerChoice = () => {
+  const selection = prompt(`${ROCK}, ${PAPER} and ${SCISSORS}`, '').toUpperCase();
+  if(selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+      alert(`Invalid choice! We choose ${DEFAULT_VALUE} for you`);
+      return DEFAULT_VALUE;
+  }
+  return selection;
+}
+
+startGameBtn.addEventListener("click", () => {
+  if(gameRunning) {
+      return 
+  }
+  console.log("Game is running");
+  gameRunning = true
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
