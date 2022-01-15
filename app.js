52e966ed9bf56aff86ffef0bb160f573b57fcaf2
Rahul Sharma <=>
2022-01-15 18:09:20 +0530

    add logic for rock paper scissor

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_VALUE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_COMPUTER_WIN = 'COMPUTER WIN';
+const RESULT_PLAYER_WIN = 'PLAYER WIN';
+
 let gameRunning = false;
+
 
 const getPlayerChoice = () => {
   const selection = prompt(`${ROCK}, ${PAPER} and ${SCISSORS}`, '').toUpperCase();
@@ -15,6 +20,27 @@ const getPlayerChoice = () => {
   return selection;
 }
 
+const getComputerChoice = () =>{
+  let randomValue = Math.random();
+  if(randomValue < 0.34) {
+    return ROCK;
+  } else if(randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+}
+const getWinner = (cChoice, pChoice) => {
+  if(cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK) {
+    alert(`computer choose ${cChoice}`);
+    return RESULT_PLAYER_WIN
+  } else {
+    alert(`computer choose ${cChoice}`);
+    return RESULT_COMPUTER_WIN
+  }
+}
 startGameBtn.addEventListener("click", () => {
   if(gameRunning) {
       return 
@@ -22,5 +48,8 @@ startGameBtn.addEventListener("click", () => {
   console.log("Game is running");
   gameRunning = true
     const playerChoice = getPlayerChoice();
-    console.log(playerChoice);
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerChoice);
+    console.log(winner);
+    gameRunning = false;
 });
